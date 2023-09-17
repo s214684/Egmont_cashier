c8d06c2c10ad690a5929d764bc0d400aa03b4573
Oliver <93136812+s214684@users.noreply.github.com>
2023-09-17 02:23:30 +0200

    added exceptions

---
+++ egmont_cashier/exceptions.py
@@ -0,0 +1,9 @@
+class FailedToReadCSV(Exception):
+    """
+    Raised when the responsible is not found in the database
+    """
+
+class FoodClubNotClosed(Exception):
+    """
+    Raised when the foodclub is not closed
+    """
